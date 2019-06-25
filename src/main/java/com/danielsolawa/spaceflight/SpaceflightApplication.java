d46a295cf9b9d40bff3065cbbbfc23ca96c04159
Daniel Solawa <>
2019-06-25 14:11:05 +0200

    First commit.

---
+++ src/main/java/com/danielsolawa/spaceflight/SpaceflightApplication.java
@@ -0,0 +1,13 @@
+package com.danielsolawa.spaceflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceflightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceflightApplication.class, args);
+    }
+
+}
